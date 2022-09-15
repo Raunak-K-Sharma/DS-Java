9e9c962265c80e9f1ed2c55d9e1b8dc17907bf70
sharmaraunak <raunak15sharma@gmail.com>
2022-09-15 08:33:55 +0530

    set up basic setup of linked list class

--- src/linkedList/SingleLinkedList.java
+++ src/linkedList/SingleLinkedList.java
@@ -1,4 +1,42 @@
 package linkedList;
 
-public class SingleLinkedList {
+import java.util.Iterator;
+
+public class SingleLinkedList<T> implements Iterable<T> {
+
+    private int size = 0;
+    private Node<T> head = null;
+    private Node<T> tail = null;
+    public static void main(String[] args) {
+
+    }
+
+    public SingleLinkedList(){
+
+    }
+
+    //clear the entire linked list
+    public void clear(){
+
+    }
+
+    public static void addNode(Object element){
+
+    }
+
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
+
+class Node<T> {
+    public T data;
+    Node<T> next;
+
+    public Node(T data){
+        this.data = data;
+        this.next = null;
+    }
 }
