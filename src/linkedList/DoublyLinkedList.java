b123089638c77cbd9948317bbc380efda8b720df
sharmaraunak <raunak15sharma@gmail.com>
2022-09-17 08:48:55 +0530

    addFirst Function tested

--- src/linkedList/DoublyLinkedList.java
+++ src/linkedList/DoublyLinkedList.java
@@ -12,7 +12,12 @@ public class DoublyLinkedList<T> implements Iterable<T> {
 
 
     public static void main(String[] args) {
-
+            DoublyLinkedList<Integer> doublyLinkedList =
+                    new DoublyLinkedList<>();
+            doublyLinkedList.addFirst(12);
+            doublyLinkedList.addFirst(14);
+            doublyLinkedList.printList();
+            System.out.println(doublyLinkedList.getSize());
     }
 
     //Empty the doublu linked list
@@ -39,6 +44,33 @@ public class DoublyLinkedList<T> implements Iterable<T> {
         return this.getSize() == 0;
     }
 
+    public void addFirst(T element){
+        if(this.isEmpty()){
+            head = new Node<>(element);
+        }else{
+            Node<T> newNode = new Node<>(element);
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        this.size += 1;
+    }
+
+    public void printList(){
+        if(this.isEmpty()){
+            System.out.println("List is Empty");
+        }
+        else{
+            Node<T> traverse = head;
+            while(traverse.next != null){
+                System.out.print(traverse.data);
+                System.out.print("->");
+                traverse = traverse.next;
+            }
+            System.out.println(traverse.data);
+        }
+    }
+
     @Override
     public Iterator<T> iterator() {
         return null;
@@ -52,6 +84,8 @@ public class DoublyLinkedList<T> implements Iterable<T> {
 
         public Node(T data){
             this.data = data;
+            this.next = null;
+            this.prev = null;
         }
 
         @Override
