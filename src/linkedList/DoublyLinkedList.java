4d3a7f026fc5612387259219b714d26820f4815d
sharmaraunak <raunak15sharma@gmail.com>
2022-09-17 08:30:18 +0530

    added clear function

--- src/linkedList/DoublyLinkedList.java
+++ src/linkedList/DoublyLinkedList.java
@@ -7,10 +7,25 @@ public class DoublyLinkedList<T> implements Iterable<T> {
 
     private Node<T> head = null;
     private Node<T> tail = null;
+    private int size = 0;
 
 
 
     public static void main(String[] args) {
+
+    }
+
+    //Empty the doublu linked list
+    public void clear(){
+        Node<T> traverse = head;
+        while(traverse != null){
+            Node<T> nextNode = traverse.next;
+            traverse.prev = traverse.next = null;
+            traverse.data = null;
+            traverse = nextNode;
+        }
+        head = tail = null;
+        size = 0;
 
     }
 
