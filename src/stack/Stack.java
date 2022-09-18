efb9fc931bb1ac5db6fb271018eb72ba71389c58
sharmaraunak <raunak15sharma@gmail.com>
2022-09-18 12:21:44 +0530

    functionalities of stack

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -1,15 +1,22 @@
 package stack;
 
+import java.util.EmptyStackException;
 import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Stack<T> implements Iterable<T> {
 
     public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+        // checking the base functions
+        System.out.println(stack.getSize());
+        System.out.println(stack.isEmpty());
+
         
     }
 
-    private LinkedList<T> stackBaseList = new LinkedList<>();
+    private final LinkedList<T> stackBaseList = new LinkedList<>();
 
     //creates an empty stack
     public Stack(){
@@ -30,11 +37,29 @@ public class Stack<T> implements Iterable<T> {
     public boolean isEmpty(){
         return this.getSize() == 0;
     }
-    private void push(T firstElement) {
+    //push an element to the stack
+    public void push(T element) {
+        stackBaseList.addLast(element);
     }
+
+    //pops an element from the stack
+    public T pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return stackBaseList.removeLast();
+    }
+
+    public T peek(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return stackBaseList.peekLast();
+    }
+
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return stackBaseList.iterator();
     }
 }
