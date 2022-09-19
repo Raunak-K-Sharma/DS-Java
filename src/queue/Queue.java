80e7f110f2e915c715532ad156ef8a8e8a405e59
sharmaraunak <raunak15sharma@gmail.com>
2022-09-19 21:33:47 +0530

    added all the functionalities of Queue

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -1,16 +1,54 @@
 package queue;
 
 import java.util.Iterator;
+import java.util.LinkedList;
 
 // implementing a Queue using a linked list
 public class Queue<T> implements Iterable<T> {
+
+    private LinkedList<T> linkedListBasedQueue = new LinkedList<>();
 
     public static void main(String[] args) {
 
     }
 
+    // normal constructor
+    public Queue () {
+
+    }
+
+    //parametrized constructor
+    public Queue( T firstElement) {
+        enqueue(firstElement);
+    }
+
+    //returns size of the queue
+    public int size(){
+        return linkedListBasedQueue.size();
+    }
+
+    //returns whether queue is empty or not
+    public boolean isEmpty(){
+        return this.size() == 0;
+    }
+
+    // adds the lement to the last of the queue
+    public void enqueue(T element) {
+        linkedListBasedQueue.addLast(element);
+    }
+
+    //removes the element from start of the queue
+    public T dequeue(T element){
+       return linkedListBasedQueue.removeFirst();
+    }
+
+    // peeks the fromt of the queue
+    public T peek(){
+        return linkedListBasedQueue.peek();
+    }
+
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return linkedListBasedQueue.iterator();
     }
 }
