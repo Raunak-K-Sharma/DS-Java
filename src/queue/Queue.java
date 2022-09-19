4801deec1918e3481d5e569d80b1b83c42b77b78
sharmaraunak <raunak15sharma@gmail.com>
2022-09-19 07:38:31 +0530

    basic setup for Queue Data Structure

---
+++ src/queue/Queue.java
@@ -0,0 +1,16 @@
+package queue;
+
+import java.util.Iterator;
+
+// implementing a Queue using a linked list
+public class Queue<T> implements Iterable<T> {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
