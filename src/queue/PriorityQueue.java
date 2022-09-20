2ea593d0d657e7a0d77b9e92b3f8bbc7d1b73db6
sharmaraunak <raunak15sharma@gmail.com>
2022-09-20 22:03:38 +0530

    intial setup of priority Queue

--- src/queue/PriorityQueue.java
+++ src/queue/PriorityQueue.java
@@ -1,6 +1,6 @@
 package queue;
 
-import java.util.List;
+import java.util.*;
 
 public class PriorityQueue<T extends Comparable<T>>{
 
@@ -13,5 +13,23 @@ public class PriorityQueue<T extends Comparable<T>>{
     //A dynamic list to keep track of items in the heap
     private List<T> heap = null;
 
+    /*
+    * This Map keeps track of alll the possible values (index
+    * value) a node can be found in the heap.Having this mapping
+    * let us all implement removal in O(logn) time with some extra
+    * space and minor overhead
+    * */
+    private Map<T, TreeSet<Integer>> positionMap = new HashMap<>();
 
+    // constructs a Priority Queue with an initial Capacity 1
+    public PriorityQueue(){
+        this(1);
+    }
+
+    //constructs a priority Queue with given HeapCapacity
+    public PriorityQueue(int heapCapacity){
+        heap = new ArrayList<>(heapCapacity);
+    }
+
+    // contructions of a heap via heapify process
 }
